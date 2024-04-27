7b7e6a35a3c8d6ac02182fd57657e00b4e9c3c54
simon.lfr <simonryom@gmail.com>
2024-04-27 16:59:10 +0200

    Attempt to update CORS.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
