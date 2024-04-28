a4e2515dd760d3e0ab905daa16e0dd381b9d90f4
simon.lfr <simonryom@gmail.com>
2024-04-28 22:36:19 +0200

    Fix CORS origin errors.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: '*',
     credentials: true,
   });
 
